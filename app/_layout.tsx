389753a2c085a02c1f9088a9ed96d7664580de82
jayawickrama <nishantharaka2004@gmail.com>
2025-02-27 23:42:52 +0530

    creating ui using tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,30 @@
-import { Stack } from 'expo-router/stack';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Tabs } from 'expo-router';
 
-export default function Layout() {
+export default function TabLayout() {
     return (
-        <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        </Stack>
+        <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Notes',
+                    tabBarIcon: ({ color }) => <FontAwesome size={28} name="sticky-note" color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name="Write new Note"
+                options={{
+                    title: 'Write',
+                    tabBarIcon: ({ color }) => <FontAwesome size={28} name="creative-commons" color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name="settings"
+                options={{
+                    title: 'User',
+                    tabBarIcon: ({ color }) => <FontAwesome size={28} name="user" color={color} />,
+                }}
+            />
+        </Tabs>
     );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,6 @@ import { View, Text, StyleSheet } from 'react-native';
 export default function Tab() {
     return (
         <View style={styles.container}>
-            <Text>Tab [Home|Settings]</Text>
         </View>
     );
 }
